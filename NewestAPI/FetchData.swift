649c46f0e9f23230ac7094a183ad5baefb7ea8d9
Paul Han <paulweitinghan@gmail.com>
2025-01-02 18:12:41 -0500

    Merge remote-tracking branch 'refs/remotes/origin/main'
    Conflicts:
    	NewestAPI.xcodeproj/project.xcworkspace/xcuserdata/s037311.xcuserdatad/UserInterfaceState.xcuserstate

--- NewestAPI/FetchData.swift
+++ NewestAPI/FetchData.swift
@@ -8,10 +8,15 @@
 import Foundation
 
 struct FetchData{
+    var response :Response = Response()
+
     mutating func getData() async{
         let URLString = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1302"
         guard let url = URL(string: URLString) else{return}
         guard let (data, _) = try? await URLSession.shared.data (from:url)else{return}
+        
+        guard let r = try? JSONDecoder().decode(Response.self, from: data)else{return}
+        response = r
         
     }
 }
@@ -36,10 +41,31 @@ extension Result: Identifiable{
 }
 
 struct FetchData2{
-    mutating func getData() async{
-        let URLString = Result.url
-        guard let url = URL(string: URLString) else{return}
+    var pokemon: Pokemon = Pokemon()
+    
+    mutating func getData(urlString: String) async{
+        guard let url = URL(string: urlString) else{return}
         guard let (data, _) = try? await URLSession.shared.data (from:url)else{return}
         
+        guard let r = try? JSONDecoder().decode(Pokemon.self, from: data)else{return}
+        pokemon = r
+        
     }
+}
+
+struct Pokemon :Codable{
+    var height : Int = 0
+    var id : Int = 0
+    var name : String? = nil
+    var sprites: Sprites = Sprites()
+    var weight: Int = 0
+}
+
+struct Sprites: Codable {
+    var showdown: Showdown = Showdown()
+}
+
+struct Showdown: Codable {
+    var frontDefault: String? = nil
+    var backDefault: String? = nil
 }
